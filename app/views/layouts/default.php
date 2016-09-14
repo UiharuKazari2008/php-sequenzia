dc521a6b367fe1e1f6b0f8d1c907894dd4a5adb2
Lain Iwakura <sysadmin@idx.ne.jp>
2016-09-14 04:59:29 -0400

    FINAL - Update News

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -47,7 +47,9 @@
 	(function() {
 	  var cookieName = 'disclaimer_accepted';
 
-	  if (Cookie.get(cookieName)) { return; }
+	  if (Cookie.get(cookieName)) { 
+		return; 
+	  }
 
 	  var $      = jQuery;
 	  var notice = 'WARNING! THIS SITE HAS MIXED CONTENT AND IS NOT SAFE FOR WORK OR PUBLIC! \n This site contains mixed content and has images containing erotic, grotesque acts, or acts of violence. NO content portrays any real life events or content illegal in the United States of America. This database is well regulated by its moderators and all posts are pending until approval. Please see SERVICE USE POLICY on ACR Docs too other policies that apply to this site. \n Content such as cate2, cate3, guro, loli, rape, and other are NOT accessible to the public or any bot unless you have an account, you must wait for a OPEN ACCESS day to register or view such content.';
