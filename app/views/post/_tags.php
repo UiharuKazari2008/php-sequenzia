6de432b3da022b7bfefc9c553176c5386bc6c140
Yuki K <UiharuKazari2008@users.noreply.github.com>
2016-09-15 00:29:39 -0400

    TESTING - Removed some section titles and edited CSS

--- app/views/post/_tags.php
+++ app/views/post/_tags.php
@@ -2,8 +2,7 @@
 !isset($this->include_tag_hover_highlight) && $this->include_tag_hover_highlight = false;
 !isset($this->include_tag_reverse_aliases) && $this->include_tag_reverse_aliases = false;
 ?>
-<div>
-  <h5><?= $this->t('.title') ?></h5>
+<div style="margin-bottom: 0.5em;>
   <ul id="tag-sidebar">
     <?php !empty($this->tags['exclude']) && print $this->tag_links($this->tags['exclude'], array('prefix' => '-', 'with_hover_highlight' => 'true', 'with_hover_highlight' => $this->include_tag_hover_highlight)) ?>
     <?php !empty($this->tags['include']) && print $this->tag_links($this->tags['include'], array('with_aliases' => $this->include_tag_reverse_aliases, 'with_hover_highlight' => $this->include_tag_hover_highlight)) ?>
