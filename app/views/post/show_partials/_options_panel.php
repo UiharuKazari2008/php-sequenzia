bd228df34100c821f2a3720e5df979ffa0117e96
Yuki K <UiharuKazari2008@users.noreply.github.com>
2016-09-15 02:25:05 -0400

    TESTING - Option Menu?

--- app/views/post/show_partials/_options_panel.php
+++ app/views/post/show_partials/_options_panel.php
@@ -25,7 +25,7 @@
 			  <?php endif ?>
 			<?php endif ?>
 		<?= $this->linkTo('■', '#', ['class' => 'submenu-button']) ?>
-		<ul class="submenu">
+		<ul class="submenu" style="display: none">
 			<li><?= $this->linkToFunction($this->t('buttons.edit'), "$('comments').hide(); $('edit').show().scrollTo(); $('post_tags').focus(); Cookie.put('show_defaults_to_edit', 1);") ?></li>
 			<!-- <?php //if (!$this->post->is_deleted() && $this->post->image() && $this->post->width && $this->post->width > 700) : ?>
 			  <li><?php //echo $this->linkToFunction($this->t('.resize'), "post->resize_image()") ?></li>
