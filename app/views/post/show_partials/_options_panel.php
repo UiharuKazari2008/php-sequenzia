2e0eaf1d220fbfaa89edb149bb29ba5874efa543
Yuki K <UiharuKazari2008@users.noreply.github.com>
2016-09-15 02:05:58 -0400

    TESTING - Option Menu?

--- app/views/post/show_partials/_options_panel.php
+++ app/views/post/show_partials/_options_panel.php
@@ -1,6 +1,28 @@
 <div>
   <ul>
-	<li class="wiki"><?php if ($this->post->has_jpeg()) : ?>
+	<li class="wiki"><?php if ($this->post->image() && $this->post->can_be_seen_by(current_user())) : ?>
+			  <?php $file_jpeg = $this->post->get_file_jpeg() ?>
+			  <?php if ($this->post->use_sample(current_user()) or current_user()->always_resize_images) : ?>
+			  <li><?php if (!array_key_exists("dakimakura", $this->post->tags()) || current_user()->is_contributor_or_higher());
+							echo $this->linkTo($this->t('.view_larger'), $file_jpeg['url'], [
+							  'class' => ($this->post->has_sample() ? "original-file-changed":"original-file-unchanged") . " highres-show",
+							  'id' => 'highres-show', 'large_width' => $this->post->width, 'large_height' => $this->post->height])
+				  ?>
+			  </li>
+			  <?php endif ?>
+			  <li><?php if (array_key_exists("dakimakura", $this->post->tags()) && !current_user()->is_contributor_or_higher()) :
+							  $file_sample = $this->post->get_file_sample(current_user());
+							  echo $this->linkTo(($this->post->has_sample() ? $this->t('.download.larger') : $this->t('.download.normal')) . ' (' . $this->numberToHumanSize($file_sample['size']) . ' ' . strtoupper($file_sample['ext']) . ')', $file_sample['url'], array(
+							  'class' => $this->post->has_sample() ? "original-file-changed":"original-file-unchanged",
+							  'id' => 'highres'));
+					  else:
+							  echo $this->linkTo(($this->post->has_sample() ? $this->t('.download.larger') : $this->t('.download.image')) . ' (' . $this->numberToHumanSize($file_jpeg['size']) . ' ' . strtoupper($file_jpeg['ext']) . ')', $file_jpeg['url'], array(
+							  'class' => ($this->post->has_sample() ? "original-file-changed":"original-file-unchanged"),
+							  'id' => 'highres'));
+					  endif
+				  ?>
+			  </li>
+			  <?php if ($this->post->has_jpeg()) : ?>
 				<?php $file_image = $this->post->get_file_image() ?>
 				<?php # If we have a JPEG, the above link was the JPEG.  Link to the PNG here. ?>
 				<li><?= $this->linkTo($this->t('.download.normal').' '.strtoupper($file_image['ext']).' ('.$this->numberToHumanSize($file_image['size']).')', $file_image['url'], array(
@@ -9,6 +31,7 @@
 						?>
 				</li>
 			  <?php endif ?>
+			<?php endif ?>
 		<?= $this->linkTo('■', '#', ['class' => 'submenu-button']) ?>
 		<ul class="submenu">
 			<li><?= $this->linkToFunction($this->t('buttons.edit'), "$('comments').hide(); $('edit').show().scrollTo(); $('post_tags').focus(); Cookie.put('show_defaults_to_edit', 1);") ?></li>
