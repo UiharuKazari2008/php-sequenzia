62921bc5c0b7414369cbe15009c6cb43814e2f05
Yuki K <UiharuKazari2008@users.noreply.github.com>
2016-09-15 01:38:18 -0400

    TESTING - Lang updates and moved a element

--- app/views/post/show_partials/_related_posts_panel.php
+++ app/views/post/show_partials/_related_posts_panel.php
@@ -1,9 +1,8 @@
 <div>
   <ul>
-    <li><?= $this->linkToIf($this->post->previous_id(), $this->t('.previous'), array('post#show', 'id' => $this->post->previous_id())) ?> <?= $this->linkToIf($this->post->next_id(), $this->t('.next'), array('post#show', 'id' => $this->post->next_id())) ?></li>
+    <li><?= $this->linkToIf($this->post->previous_id(), $this->t('.previous'), array('post#show', 'id' => $this->post->previous_id())) ?> <?= $this->linkToIf($this->post->next_id(), $this->t('.next'), array('post#show', 'id' => $this->post->next_id())) ?> <?= $this->linkTo($this->t('.random'), 'post#random') ?></li>
     <?php if ($this->post->parent_id) : ?>
       <li><?= $this->linkTo($this->t('.parent'), array('post#show', 'id' => $this->post->parent_id)) ?></li>
     <?php endif ?>
-    <li><?= $this->linkTo($this->t('.random'), 'post#random') ?></li>
   </ul>
 </div>
