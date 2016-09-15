e51dad433be03d726fadc94730436ff151a7a940
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2016-09-15 17:52:42 -0400

    SMALL MOD - Updated News

--- app/views/layouts/_news.php
+++ app/views/layouts/_news.php
@@ -1,7 +1,8 @@
 <?php if (!CONFIG()->enable_news_ticker) return; ?>
 <div id="news-ticker" style="display: none">
   <ul>
-    <li>Sequenzia @ <a href="https://code.acr.moe/kazari/myimouto">ACR Code</a>, 100% Open! NEW! Ultra Dense Layout! More Content, Less Waste!</li>
+    <li>Sequenzia @ <a href="https://code.acr.moe/kazari/myimouto">ACR Code</a>! Feel free to modify and fork your own server!!</li>
+    <li>NEW! Ultra compact layout! More Content, Less Waste! More updates to colors! Please leave feedback!!</li>
   </ul>
 
   <a href="#" id="close-news-ticker-link"><?= $this->t('.close') ?></a>
