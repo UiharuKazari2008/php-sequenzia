370c0228578a6b6b5352e8f0ff5538ae6e0a1385
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2016-09-15 22:31:13 -0400

    TESTING - Title Bar Search

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -132,7 +132,16 @@ container
 </script>
   <?= $this->partial('layouts/news') ?>
   <div id="header">
-    <div id="title"><h2 id="site-title"><?= $this->linkTo($this->imageTag('images/logo_small.png', array('alt' => CONFIG()->app_name, 'size' => '195x40', 'id' => 'logo')), CONFIG()->url_base) ?> <?= $this->partial('search') ?></h2></div>
+    <div id="title"><h2 id="site-title"><?= $this->linkTo($this->imageTag('images/logo_small.png', array('alt' => CONFIG()->app_name, 'size' => '195x40', 'id' => 'logo')), CONFIG()->url_base) ?><div style="margin-right: 0.0em;">
+  <?= $this->formTag('post#index', array('method' => 'get', 'accept-charset' => 'UTF-8'), function(){ ?>
+    <div style="margin:0;padding:0;display:inline"></div>
+    <div>
+      <?= $this->textFieldTag("tags", $this->h($this->params()->tags), array('size' => '38', 'autocomplete' => 'off', 'style' => 'font-size: 20pt; padding: 0 0 1px 4px; margin: 4px 0 0 8px; border-style: none; background: #2b0000;')) ?>
+      <?= $this->submitTag($this->t('.search'), array('style' => 'display: none;', 'name' => '')) ?>
+    </div>
+  <?php }) ?>
+</div>
+<?= $this->tag_completion_box('$("tags")', ['$("tags").up("form")', '$("tags")', null], true) ?> </h2></div>
     <?= $this->partial('layouts/menu') ?>
   </div>
   <?= $this->partial('layouts/login') ?>
