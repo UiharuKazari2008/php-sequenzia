08125bf055524da219d2ea775660a6bee81ead42
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2016-09-15 21:53:22 -0400

    TESTING - Title Bar Search

--- app/views/post/_search.php
+++ app/views/post/_search.php
@@ -2,7 +2,7 @@
   <?= $this->formTag('post#index', array('method' => 'get', 'accept-charset' => 'UTF-8'), function(){ ?>
     <div style="margin:0;padding:0;display:inline"></div>
     <div>
-      <?= $this->textFieldTag("tags", $this->h($this->params()->tags), array('size' => '20', 'autocomplete' => 'off')) ?>
+      <?= $this->textFieldTag("tags", $this->h($this->params()->tags), array('size' => '50', 'autocomplete' => 'off', 'style' => 'font-size: 18pt; padding: 0px; margin: 0 0 0 8px ;')) ?>
       <?= $this->submitTag($this->t('.search'), array('style' => 'display: none;', 'name' => '')) ?>
     </div>
   <?php }) ?>
