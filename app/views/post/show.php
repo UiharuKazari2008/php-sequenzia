9594818e9c44558ad93ba8edf96a98668a3f3a01
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2016-09-17 15:23:06 -0400

    TESTING - Menu

--- app/views/post/show.php
+++ app/views/post/show.php
@@ -12,7 +12,7 @@
     <?php endif ?>
 
     <?= $this->partial('post/show_partials/status_notices', array('pools' => $this->pools)) ?>
-    <div class="clearfix" id="main-menu">
+    <div class="clearfix" id="main-menu" style="list-style-type:none;margin:0">
 	  <?= $this->partial('post/show_partials/related_posts_panel') ?> <?= $this->partial('tags') ?> <?= $this->partial('post/show_partials/statistics_panel') ?> <?= $this->partial('post/show_partials/options_panel') ?>
 	  <?php if (CONFIG()->can_show_ad('post#show-sidebar', current_user())) : ?>
 		<?= $this->partial('vertical') ?>
