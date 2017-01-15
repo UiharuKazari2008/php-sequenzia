da8177e024f41381e7c168bfe512eaea56aa9ca6
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2017-01-15 08:12:33 -0500

    Update _news.php

--- app/views/layouts/_news.php
+++ app/views/layouts/_news.php
@@ -1,8 +1,7 @@
 <?php if (!CONFIG()->enable_news_ticker) return; ?>
 <div id="news-ticker" style="display: none">
   <ul>
-    <li>Sequenzia @ <a href="https://code.acr.moe/kazari/myimouto">ACR Code</a>! Feel free to modify and fork your own server!!</li>
-    <li>NEW! Ultra compact layout! More Content, Less Waste! More updates to colors! Please leave feedback!!</li>
+    <li>Come visit Winterchan (<a href="https://9854.moe">HTTPS</a>)!</li>
   </ul>
 
   <a href="#" id="close-news-ticker-link"><?= $this->t('.close') ?></a>
