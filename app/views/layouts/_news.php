d18f5bb167dcebc97a009e554eedbd3762665374
Yukimi Kazari <divine-exseed-freya-megna@live.com>
2017-06-06 12:02:34 -0400

    Small changes

--- app/views/layouts/_news.php
+++ app/views/layouts/_news.php
@@ -1,7 +1,7 @@
 <?php if (!CONFIG()->enable_news_ticker) return; ?>
 <div id="news-ticker" style="display: none">
   <ul>
-    <li><a href="https://9854.moe">Winterchan</a> - <i>Jan 26</i>: Updated UI!</li>
+    <li><a href="https://9854.moe">Visit Winterchan</a></li>
   </ul>
 
   <a href="#" id="close-news-ticker-link"><?= $this->t('.close') ?></a>
